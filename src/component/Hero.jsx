a9686fcbab63b481881d379709852bfbcd1a58a3
Santosh Mehta <mehtasantosh838@gmail.com>
2025-07-09 18:50:13 +0530

    video link chnage

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -101,7 +101,7 @@ const Hero = () => {
       <div className="video absolute inset-0">
         <video
           ref={videoRef}
-          src="/videos/output.mp4"
+          src="/public/videos/output.mp4"
           muted
           playsInline
           preload="auto"
